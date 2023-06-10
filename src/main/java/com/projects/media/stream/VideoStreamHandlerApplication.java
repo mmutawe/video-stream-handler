0e4150d70a9bb69a89a142e803dd46c846518917
mmutawe <mahmoudmutawe@gmail.com>
2023-06-10 16:55:19 -0500

    Initial commit

---
+++ src/main/java/com/projects/media/stream/VideoStreamHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.projects.media.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStreamHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/media/stream/VideoStreamHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.media.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
